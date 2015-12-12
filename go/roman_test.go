8fe8489b6bca084fdbbe5519feda6e1c0c95ec5d
Phongsak Ritpitakphong <jane.makub@gmail.com>
2015-12-13 00:36:42 +0700

    add case to 7

--- go/roman_test.go
+++ go/roman_test.go
@@ -2,23 +2,34 @@ package converter
 
 import "testing"
 
-const (
-	ONE = "I"
-)
-
 func TestInput1ShouldGetI(t *testing.T) {
 	result := roman(1)
-	errorCheck(t, result, ONE)
-}
-
-func TestInput2ShoudGetII(t *testing.T) {
-	result := roman(2)
-	errorCheck(t, result, "II")
+	errorCheck(t, result, "I")
 }
 
 func TestInput3ShoudGetIII(t *testing.T) {
 	result := roman(3)
 	errorCheck(t, result, "III")
+}
+
+func Test4ShouldGetIX(t *testing.T) {
+	result := roman(4)
+	errorCheck(t, result, "IV")
+}
+
+func Test5ShouldGetX(t *testing.T) {
+	result := roman(5)
+	errorCheck(t, result, "V")
+}
+
+func Test6ShouldGetX(t *testing.T) {
+	result := roman(6)
+	errorCheck(t, result, "VI")
+}
+
+func Test7ShouldGetVII(t *testing.T) {
+	result := roman(7)
+	errorCheck(t, result, "VII")
 }
 
 func errorCheck(t *testing.T, result, expected string) {
@@ -28,9 +39,22 @@ func errorCheck(t *testing.T, result, expected string) {
 }
 
 func roman(n int) string {
+
+	romanMap := map[int]string{
+		1: "I",
+		4: "IV",
+		5: "V",
+		6: "VI",
+		7: "VII",
+	}
+	if _, ok := romanMap[n]; ok {
+		return romanMap[n]
+	}
+
 	result := ""
 	for i := 0; i < n; i++ {
-		result = result + ONE
+		result = result + romanMap[1]
 	}
+
 	return result
 }
